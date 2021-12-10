0b915e960c1b8e6c34cddae25ac435dedbafb2b8
mansi_829 <mansigundre1@gmail.com>
2021-12-10 17:06:30 +0530

    1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import FormPage from './components/Login';
+import NavbarPage from './components/Navbar'
+    return (
+      <Router>
+        <div className="App">
+          <FormPage />
+          <NavbarPage/>
+        </div>
+      </Router>
+    );
+  
+
+export default App;
