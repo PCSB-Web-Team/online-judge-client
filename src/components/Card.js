2fc4c381bfb6f84ffc8399afb5d4f39d44378952
mansi_829 <mansigundre1@gmail.com>
2022-01-17 17:56:53 +0530

    minor-change

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,29 +5,14 @@ import { Requests } from "../../src/utils/Index";
 
 const Card = (props) =>
 {
-    const navigate = useNavigate();
 
-    function handleClick(props)
-    {
-        Requests.contest(data).then((res) =>
-        {
-            props.log(res.data);
-        })
-        const token = localStorage.getItem("pcsb-oj-token");
-        if (token)
-        {
-            navigate(`/contest/${ id }`)
-        }
-        navigate("/login")
-
-    }
     return (
         <div className="card p-8 m-2 shadow">
             <img src={caret} alt="contest" className="h-32 sm:h-48 w-full object-cover" />
             <div className="m-4">
                 <span className="font-bold">Contest {props.id}</span>
                 <span className="block text-gray-500 text-sm">{props.description}</span><br />
-                <input type="button" value="Enter Contest" onClick={handleClick} className='btn_1' />
+                <input type="button" value="Enter Contest" type="submit" className='btn_1' />
             </div>
             <div className="badge">
                 <span>{props.date}</span>
--- src/pages/DashBoard/DashBoard.js
+++ src/pages/DashBoard/DashBoard.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import { connect } from "react-redux";
 import { useParams } from 'react-router'
+import { useNavigate } from 'react-router';
 import Card from '../../components/Card';
+import { Requests } from '../../utils/Index';
+import { contest } from '../../utils/Requests';
 
-
-const Dashboard = () =>
+const Dashboard = (props) =>
 {
-
+  const navigate = useNavigate();
   return (
-    <div>
+
+    <div onSubmit={async values =>
+    {
+      Requests.contest(values).then(res =>
+      {
+        props.contest(res.data.id);
+        navigate(`/contest/`)
+      })
+    }
+    }>
       <div className="container "><br /><br />
         <h1 class="font-bold pb-2 p-8 border-b border-gray-200">Contest</h1>
         <div class="mt-8 grid m-12 lg:grid-cols-3 gap-10">
@@ -20,4 +32,17 @@ const Dashboard = () =>
   )
 }
 
-export default Dashboard
+function mapStateToProps(state)
+{
+  return {
+    isAuthenticated: state.isAuthenticated
+  }
+}
+function mapActionToProps(dispatch)
+{
+  return {
+    contest: (userData) => dispatch(contest(userData))
+  }
+}
+
+export default connect(mapStateToProps, mapActionToProps)(Dashboard);
