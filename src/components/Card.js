6a1482b80dfdff4ab8fbd1fe0e67622d4e9851ae
Vedant Daigavane <vedant.daigavane@rksv.in>
2022-01-31 21:36:52 +0530

    status bug solved

--- src/components/Card.js
+++ src/components/Card.js
@@ -44,7 +44,7 @@ const Card = ({ status, ...props }) => {
           <Link to={props.isAuthenticated ? `/${props.contestId}` : "/login"}>
             <button
               className="text-white bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-              disabled={status.time}
+              disabled={!status.time}
             >
               Enter Contest
             </button>
