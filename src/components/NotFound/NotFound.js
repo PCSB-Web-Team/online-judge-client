3b742db0c49a534f477f21e9caf49ccc1dace4c3
mansi0829 <mansigundre1@gmail.com>
2022-02-08 10:23:40 +0530

    404 Page Fixed

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,31 @@
 import React from "react";
-import "./NotFound.css";
+import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
     <div className="mb-4">
-      <h1 className="text-lg mb-4">Page not found</h1>
+      <div class="p-24 text-2xl">
+        <div className="mb-4">// 404 page not found.</div>
+        <div className="flex">
+          <div>if</div>(<div>!</div>
+          <div>found</div>)
+        </div>
+        <div>
+          <div>
+            <i></i>throw
+          </div>
+          <div>
+            (<span>"(╯°□°)╯︵ ┻━┻"</span>);
+          </div>
+          <div className="block mb-4"></div>
+          <div>
+            //
+            <Link to="/" className="text-cyan-500">
+              Go home!
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
