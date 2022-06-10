48095234c8d1095ee495ab4bed4c37a8c6561f31
mansi0829 <mansigundre1@gmail.com>
2022-06-11 00:50:37 +0530

    Revert "minor changes"
    This reverts commit fc78a31b4ea144ea7d10780843af66bec7b988cf.

--- src/pages/Submission/AllSubmission.js
+++ src/pages/Submission/AllSubmission.js
@@ -35,14 +35,12 @@ function AllSubmission() {
       button: true,
       cell: (row) => (
         <div className="text-center">
-          <Link to={`${row._id}`}>
-            <button
-              type="button"
-              className="text-white bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-800 dark:border-gray-700"
-            >
-              View
-            </button>
-          </Link>
+          <button
+            type="button"
+            className="text-white bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-800 dark:border-gray-700"
+          >
+            <Link to={`${row._id}`}>View</Link>
+          </button>
         </div>
       ),
     },
